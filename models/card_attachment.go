4a3040d54e383c01430fac4adc4aa229117f320d
meidhika <meidhikanawa@gmail.com>
2026-04-04 19:55:22 +0700

    card repository

--- models/card_attachment.go
+++ models/card_attachment.go
@@ -14,5 +14,9 @@ type CardAttachment struct {
 	File string `json:"file" db:"file"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 
+	FileURL string `json:"file_url" gorm:"-"`
+}
 
+func (CardAttachment) TableName() string {
+	return "card_attachment"
 }
---
+++ repositories/card_repository.go
@@ -0,0 +1,66 @@
+package repositories
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/meidhika/project-management/config"
+	"github.com/meidhika/project-management/models"
+	"gorm.io/gorm"
+)
+
+type CardRepository interface {
+	Create(card *models.Card) error
+	Update(card *models.Card) error
+	Delete(id uint) error
+	FindByID(id uint) (*models.Card, error)
+	FindByPublicID(publicID string) (*models.Card, error)
+	FindByListID(listID uint) ([]models.Card, error)
+}
+type cardRepository struct {
+}
+
+func NewCardRepository() CardRepository {
+	return &cardRepository{}
+}
+
+func (r *cardRepository) Create(card *models.Card) error {
+	return config.DB.Create(card).Error
+}
+
+func (r *cardRepository) Update(card *models.Card) error {
+	return config.DB.Save(card).Error
+}
+
+func (r *cardRepository) Delete(id uint) error {
+	return config.DB.Delete(&models.Card{}, id).Error
+}
+
+func (r *cardRepository) FindByID(id uint) (*models.Card, error) {
+	var card models.Card
+	err := config.DB.Preload("Labels").Preload("Assigness").First(&card, id).Error
+	return &card, err
+}
+
+func (r *cardRepository) FindByPublicID(publicID string) (*models.Card, error) {
+	var card models.Card
+	if err := config.DB.Preload("Assigness.User", func (tx *gorm.DB) *gorm.DB {
+		return tx.Select("internal_id, public_id, name, email")
+	}).Preload("Attachments").Where("public_id = ?", publicID).First(&card).Error; err != nil {
+		return nil, err
+	}
+
+	baseUrl := config.AppConfig.APPURL
+	for i := range card.Attachments {
+		card.Attachments[i].FileURL = fmt.Sprintf("%s/files/%s", baseUrl, filepath.Base(card.Attachments[i].File))
+	}
+
+	return &card, nil
+}
+
+func (r *cardRepository) FindByListID(listID uint) ([]models.Card, error) {
+	var cards []models.Card
+	err := config.DB.Joins("JOIN lists ON lists.internal_id = cards.list_internal_id").Where("list.public_id = ?", listID).Order("position ASC").Find(&cards).Error
+
+	return cards, err
+}
